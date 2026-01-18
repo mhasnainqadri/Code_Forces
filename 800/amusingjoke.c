fc7563df79d7ddf87d78b99782f1b547238f07e8
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-18 20:38:29 +0500

    code

--- 800/amusingjoke.c
+++ 800/amusingjoke.c
@@ -2,41 +2,24 @@
 #include<stdbool.h>
 
 // Function Declarations :
-void stringMerger(char *str1,char *str2);
-bool areAnagrams(char *str1,char *str2);
+bool solution(const char *str1,const char *str2,const char *str3);
 
 // Main Function :
 int main()
 {
-    char string1[101],string2[101],string3[101]; // Declaring String Variables
-    scanf("%s%s%s",string1,string2,string3); // Reading Strings
-    stringMerger(string1,string2); // Passing Strings 1 & 2 To String-Merging Function
-    bool answer=areAnagrams(string1,string3); // Passing Merged-String & String 3 to Anagrams Checking Function
-    printf("%s",(answer==true)?"YES":"NO"); // Displaying Result
+    char string1[101],string2[101],string3[202]; // Declaring String Variables
+    scanf("%100s%100s%201s",string1,string2,string3); // Reading Strings
+    printf("%s",(solution(string1,string2,string3)==true)?"YES":"NO"); // Displaying Result
     return 0;
 }
 
 // Function Definitions :
-void stringMerger(char *str1,char *str2)
+bool solution(const char *str1,const char *str2,const char *str3)
 {
-    int len1=0,len2=0;
-    for(int i=0;str1[i]!='\0';i++) len1++;
-    for(int i=0;str2[i]!='\0';i++) len2++;
-    for(int i=0;i<len2;i++) str1[len1+i]=str2[i];
-    str1[len1+len2]='\0';
-}
-bool areAnagrams(char *str1,char *str2)
-{
-    int len1=0,len2=0;
-    for(int i=0;str1[i]!='\0';i++) len1++;
-    for(int i=0;str2[i]!='\0';i++) len2++;
-    if(len1!=len2) return false;
     int freq[256]={0};
-    for(int i=0;i<len1;i++)
-    {
-        freq[(unsigned char)str1[i]]++;
-        freq[(unsigned char)str2[i]]--;
-    }
+    for(int i=0;str1[i]!='\0';i++) freq[(unsigned char)str1[i]]++;
+    for(int i=0;str2[i]!='\0';i++) freq[(unsigned char)str2[i]]++;
+    for(int i=0;str3[i]!='\0';i++) freq[(unsigned char)str3[i]]--;
     for(int i=0;i<256;i++)
         if(freq[i]!=0)
             return false;

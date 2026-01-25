ffb858b7dbb808905f7dc2eaa5fccc2ff33b561e
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-25 14:31:53 +0500

    code

--- 800/beautifulmatrix.c
+++ 800/beautifulmatrix.c
@@ -2,7 +2,7 @@
 #include<math.h>
 int main()
 {
-    int a[5][5],x,y,moves;
+    int a[5][5],moves;
     for(int i=0;i<5;i++)
     {
         for(int j=0;j<5;j++)
@@ -10,11 +10,9 @@ int main()
             scanf("%d",&a[i][j]);
             if(a[i][j]==1)
             {
-                x=i-2;
-                y=j-2;
+                moves=abs(i-2)+abs(j-2);
             }
         }
-        moves=abs(x)+abs(y);
     }
     printf("%d",moves);
     return 0;
